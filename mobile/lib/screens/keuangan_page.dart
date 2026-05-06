e91676a53b6ddae419e774aff1a770e983b63a6f
mmei203 <meisyandaryani@gmail.com>
2026-05-06 19:42:07 +0800

    ganti nama folder

---
+++ mobile/lib/screens/keuangan_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class KeuanganPage extends StatefulWidget {
+  const KeuanganPage({super.key});
+
+  @override
+  State<KeuanganPage> createState() => _KeuanganPageState();
+}
+
+class _KeuanganPageState extends State<KeuanganPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Keuangan'),
+    );
+  }
+}
