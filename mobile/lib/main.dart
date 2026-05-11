a870e6c00c1576f5423e1506298b8d5381a49001
mmei203 <meisyandaryani@gmail.com>
2026-05-11 21:17:21 +0800

    ganti font ke poppins

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:mobile/screens/home_page.dart';
 import 'package:mobile/screens/login_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mobile/screens/profile_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   await dotenv.load();
@@ -21,7 +22,10 @@ class MyApp extends StatelessWidget {
       navigatorKey: navigatorKey,
       title: 'Admin Keuangan',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: false),
+      theme: ThemeData(useMaterial3: false,
+      textTheme: GoogleFonts.poppinsTextTheme(
+        Theme.of(context).textTheme
+      )),
 
       // routes
       initialRoute: '/',
