02f1b2bec84844f2e4760972f2b52014a45f83e4
mmei203 <meisyandaryani@gmail.com>
2026-05-11 14:15:19 +0800

    tambah home page, appbar

---
+++ mobile/lib/screens/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/utils/config.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Preset.primaryColor,
+        leadingWidth: 70,
+        elevation: 0,
+        leading: Container(
+          padding: EdgeInsets.only(left: 20, bottom: 10),
+          child: Image.asset('assets/logo.png'),
+        ),
+        centerTitle: true,
+        title: Text(
+          'SIMPADU',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w800
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: (){}, icon: Icon(Icons.notifications_outlined)
+          ),
+          IconButton(
+            onPressed: (){}, icon: Icon(Icons.account_circle_outlined)
+          ),
+        ],
+      ),
+    );
+  }
+}
