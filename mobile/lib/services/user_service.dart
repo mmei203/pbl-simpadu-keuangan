36f0f227e0f268788f54b7e7962e11c7e7cca719
mmei203 <meisyandaryani@gmail.com>
2026-05-12 19:42:24 +0800

    tambah get user

--- mobile/lib/services/user_service.dart
+++ mobile/lib/services/user_service.dart
@@ -7,6 +7,7 @@ import 'package:http/http.dart' as http;
 class UserService {
   final String url = dotenv.get("BASE_URL");
 
+  // create user
   Future<bool> createUser(UserRequests request) async {
     try {
       final response = await http.post(
@@ -25,6 +26,29 @@ class UserService {
     } catch (e) {
       debugPrint(e.toString());
       return false;
+    }
+  }
+
+  // get user
+  Future<List<UserResponse>?> getDataUser() async {
+    try {
+      final response = await http.get(Uri.parse("$url/user"));
+
+      if (response.statusCode == 200) {
+        final jsonResponse = await jsonDecode(response.body);
+
+        debugPrint('hit API: $jsonResponse');
+
+        final List<UserResponse> data = (jsonResponse as List)
+            .map((e) => UserResponse.fromJson(e))
+            .toList();
+
+        return data;
+      }
+      return null;
+    } catch (e) {
+      debugPrint(e.toString());
+      return null;
     }
   }
 }
