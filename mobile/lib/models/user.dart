477abc2641f06e53c23eb339e8744b07bced0b16
CursedDynn <94917626+AdynSlebew66@users.noreply.github.com>
2026-05-13 07:53:02 +0800

    Delete mobile directory

--- mobile/lib/models/user.dart
+++
@@ -1,8 +0,0 @@
-class UserRequests {
-  String email;
-  String password;
-
-  UserRequests({required this.email, required this.password});
-
-  Map<dynamic, String> toJson() => {"email": email, "password": password};
-}
