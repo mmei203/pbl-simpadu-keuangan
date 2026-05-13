48e20bc7dd02ccad43ad17c7765d052956a63c02
mmei203 <meisyandaryani@gmail.com>
2026-05-13 21:54:29 +0800

    tambah pindah class AppText ke config.dart

--- mobile/lib/utils/config.dart
+++ mobile/lib/utils/config.dart
@@ -52,3 +52,23 @@ class Preset {
     )
   );
 }
+
+class AppText {
+  static final myText = {
+    'welcome': 'Selamat Datang',
+    'login': 'Silahkan Login',
+    'total-mhs': 'Total Mahasiswa',
+    'bayar-mhs': 'Sudah Bayar',
+    'cicil-mhs': 'Sedang Mencicil',
+    'blm-bayar-mhs': 'Belum Bayar',
+    'notif': 'Pemberitahuan',
+    'nim': 'NIM',
+    'nama-mhs': 'Nama Mahasiswa',
+    'jurusan-mhs': 'Jurusan',
+    'ukt': 'UKT',
+    'stat': 'Status',
+    'act': 'Aksi',
+    'beasiswa': 'Beasiswa',
+    'cicil': 'Sisa Cicilan',
+  };
+}
