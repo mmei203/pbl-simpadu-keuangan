724b818ba66ecf7343c99d2d3d97909223504f6c
mmei203 <meisyandaryani@gmail.com>
2026-05-14 16:59:45 +0800

    ganti warna latar

--- mobile/lib/screens/login_page.dart
+++ mobile/lib/screens/login_page.dart
@@ -13,7 +13,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Preset.saveColor,
+      backgroundColor: Preset.primaryColor,
       body:Center(
         child: SingleChildScrollView(
           child: SizedBox(
