cc0c0eb2a56b671203cec2946c1147df62124e16
mmei203 <meisyandaryani@gmail.com>
2026-05-16 15:43:54 +0800

    membuat halaman pembayaran

--- mobile/lib/screens/pembayaran_page.dart
+++ mobile/lib/screens/pembayaran_page.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mobile/components/search.dart';
+import 'package:mobile/components/tabel.dart';
+import 'package:mobile/models/mahasiswa.dart';
+import 'package:mobile/utils/config.dart';
 
-class PembayaranPage extends StatelessWidget {
+class PembayaranPage extends StatefulWidget {
   const PembayaranPage({super.key});
 
   @override
+  State<PembayaranPage> createState() => _PembayaranPageState();
+}
+
+class _PembayaranPageState extends State<PembayaranPage> {
+  final TextEditingController _searchController = TextEditingController();
+
+  void _executeSearch() {
+    String query = _searchController.text;
+    if (query.isNotEmpty) {
+      print('cari $query');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Preset.backgroundColor,
+        leading: IconButton(
+          icon: FaIcon(FontAwesomeIcons.angleLeft, color: Colors.black),
+          onPressed: () {
+            print('kembali ke awal');
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('Kelola Pembayaran', style: TextStyle(color: Colors.black)),
+      ),
+      body: Container(
+        color: Preset.backgroundColor,
+        padding: EdgeInsets.only(left: 15, right: 15, top: 15, bottom: 90),
+        child: Column(
+          children: [
+            // cari mahasiswa
+            Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: FractionallySizedBox(
+                widthFactor: 0.65,
+                child: SizedBox(
+                  height: 45,
+                  child: CariMhs(
+                    controller: _searchController,
+                    onSearch: _executeSearch,
+                  ),
+                ),
+              ),
+            ),
+            Preset.smallSpace,
+            // Tabel UKT
+            Expanded(
+              child: TabelMahasiswa(data: dummyPembayaran, labelStatus: "Status"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
