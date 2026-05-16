b08ddf9723e8b875d16c931abd2ec17f8f79dcac
mmei203 <meisyandaryani@gmail.com>
2026-05-16 15:43:37 +0800

    membuat halaman cicilan

--- mobile/lib/screens/cicilan_page.dart
+++ mobile/lib/screens/cicilan_page.dart
@@ -1,10 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mobile/components/addbtn.dart';
+import 'package:mobile/components/search.dart';
+import 'package:mobile/components/tabel.dart';
+import 'package:mobile/models/mahasiswa.dart';
+import 'package:mobile/utils/config.dart';
 
-class CicilanPage extends StatelessWidget {
+class CicilanPage extends StatefulWidget {
   const CicilanPage({super.key});
 
   @override
+  State<CicilanPage> createState() => _CicilanPageState();
+}
+
+class _CicilanPageState extends State<CicilanPage> {
+  final TextEditingController _searchController = TextEditingController();
+
+  void _executeSearch() {
+    String query = _searchController.text;
+    if (query.isNotEmpty) {
+      print('cari $query');
+    }
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.black,
+        elevation: 2,
+        backgroundColor: Preset.backgroundColor,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const FaIcon(FontAwesomeIcons.angleLeft),
+        ),
+        title: const Text('Kelola Cicilan'),
+      ),
+      backgroundColor: Preset.backgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8, left: 8, right: 8, bottom: 60),
+        child: Column(
+          children: [
+            // Bagian search
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Align(
+                      alignment: AlignmentDirectional.centerStart,
+                      child: FractionallySizedBox(
+                        widthFactor: 0.75,
+                        child: SizedBox(
+                          height: 45,
+                          child: CariMhs(
+                            controller: _searchController,
+                            onSearch: _executeSearch,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  // tambah mhs
+                  Addbtn(),
+                ],
+              ),
+            ),
+            Expanded(
+              child: TabelMahasiswa(
+                data: dummyCicilan,
+                labelStatus: "Sisa Cicilan",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
