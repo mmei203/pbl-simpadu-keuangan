79b750cbc3042e245a627e4943697404de624b2c
mmei203 <meisyandaryani@gmail.com>
2026-05-22 20:26:34 +0800

    ganti method user

--- mobile/lib/services/user_service.dart
+++ mobile/lib/services/user_service.dart
@@ -7,45 +7,23 @@ import 'package:http/http.dart' as http;
 class UserService {
   final String url = dotenv.get("BASE_URL");
 
-  // create user
-  Future<bool> createUser(UserRequests request) async {
+  // Ganti method createUser
+  Future<UserResponse?> login(UserRequests request) async {
     try {
       final response = await http.post(
         Uri.parse("$url/api/auth/login"),
         body: jsonEncode(request.toJson()),
-        headers: {"accept": "application/json"},
+        headers: {
+          "accept": "application/json",
+          "Content-Type": "application/json",
+        },
       );
-      final jsonResponse = await jsonDecode(response.body);
+      final jsonResponse = jsonDecode(response.body);
       debugPrint("Hit API: $jsonResponse");
 
-      if (response.statusCode != 200) {
-        debugPrint("macam aneh jo");
-        return false;
-      }
-      return true;
-    } catch (e) {
-      debugPrint(e.toString());
-      return false;
-    }
-  }
+      if (response.statusCode != 200) return null;
 
-  // get user
-  Future<List<UserResponse>?> getDataUser() async {
-    try {
-      final response = await http.get(Uri.parse("$url/user"));
-
-      if (response.statusCode == 200) {
-        final jsonResponse = await jsonDecode(response.body);
-
-        debugPrint('hit API: $jsonResponse');
-
-        final List<UserResponse> data = (jsonResponse as List)
-            .map((e) => UserResponse.fromJson(e))
-            .toList();
-
-        return data;
-      }
-      return null;
+      return UserResponse.fromJson(jsonResponse['data']);
     } catch (e) {
       debugPrint(e.toString());
       return null;
