e6344cdfe26acc91aaa7a23334367e95580b9899
mmei203 <meisyandaryani@gmail.com>
2026-05-22 20:27:08 +0800

    get username

--- mobile/lib/providers/user_provider.dart
+++ mobile/lib/providers/user_provider.dart
@@ -5,15 +5,22 @@ import 'package:mobile/services/user_service.dart';
 class UserProvider with ChangeNotifier {
   final UserService userService = UserService();
   bool isLoading = false;
+  UserResponse? _loggedInUser;
+  UserResponse? get loggedInUser => _loggedInUser;
   List<UserResponse>? _data;
   List<UserResponse>? get data => _data;
+
+  // get user name
+  void setLoggedInUser(UserResponse user) {
+    _loggedInUser = user;
+    notifyListeners();
+  }
 
   Future<void> getAllUser() async {
     isLoading = true;
     notifyListeners();
 
     try {
-      _data = await userService.getDataUser();
       isLoading = false;
       notifyListeners();
     } catch (e) {
