5ed8c868a96a86217bedf9e646171fd5ca35fa2c
mmei203 <meisyandaryani@gmail.com>
2026-05-26 20:11:08 +0800

    ulang halaman kelola ukt

---
+++ mobile/lib/screens/page-kelola/ukt_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mobile/components/search.dart';
+import 'package:mobile/utils/config.dart';
+
+class UktPage extends StatelessWidget {
+  const UktPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // back button
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: FaIcon(FontAwesomeIcons.angleLeft),
+        ),
+        // title
+        title: Text('Kelola UKT'),
+        elevation: 2,
+        backgroundColor: Preset.primaryColor,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            // cari mahasiswa
+            Search(),
+            // tabel
+          ],
+        ),
+      ),
+    );
+  }
+}
