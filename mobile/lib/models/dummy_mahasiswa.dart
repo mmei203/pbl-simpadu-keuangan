7557a4f87979af22113fdb7b671f893baaec66f2
mmei203 <meisyandaryani@gmail.com>
2026-06-12 20:17:54 +0800

    tambah dummy mahasiswa

---
+++ mobile/lib/models/dummy_mahasiswa.dart
@@ -0,0 +1,21 @@
+class Mahasiswa {
+  final String nim;
+  final String nama;
+  final String prodi;
+  String ukt;
+
+  Mahasiswa({
+    required this.nim,
+    required this.nama,
+    required this.prodi,
+    required this.ukt,
+  });
+}
+
+// Dummy Data Awal
+List<Mahasiswa> dummyMahasiswa = [
+  Mahasiswa(nim: 'C030324122', nama: 'Budi', prodi: 'Teknik Informatika', ukt: 'UKT 1'),
+  Mahasiswa(nim: 'C030324123', nama: 'Siti', prodi: 'Teknik Informatika', ukt: 'UKT 2'),
+  Mahasiswa(nim: 'C030324124', nama: 'Andi', prodi: 'Teknik Informatika', ukt: 'UKT 3'),
+  Mahasiswa(nim: 'C030324125', nama: 'Rani', prodi: 'Teknik Informatika', ukt: 'UKT 3'),
+];
